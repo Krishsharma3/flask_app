a7e775b6cb0eabf826bf3bfc5dfd327d4532b655
Krishsharma3 <krishsharma3@gmail.com>
2025-03-12 10:38:54 +0530

    Initial commit

--- from flask import Flask.py
+++ from flask import Flask.py
@@ -8,3 +8,12 @@ def hello():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
+
+cd D:\DSML\flask_app
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/Krishsharma3/FlaskApp.git
+git push -u origin main
