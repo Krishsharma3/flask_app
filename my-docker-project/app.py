1638bb46cd43567dea8f39cdd3837360bad45779
Krishsharma3 <krishsharma3@gmail.com>
2025-03-12 16:45:44 +0530

    Initial commit

--- my-docker-project/app.py
+++ my-docker-project/app.py
@@ -7,6 +7,4 @@ def home():
     return "Hello, Dockerized Flask App!"
 
 if __name__ == "__main__":
-    # Make sure to listen on all interfaces
     app.run(host="0.0.0.0", port=5000, debug=True)
-
